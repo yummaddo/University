482acca9d5fc8fd196a9e618424f39e21132f614
óchiAnthropos <58050924+ochiantropos@users.noreply.github.com>
2022-09-05 12:41:28 +0300

    static lib (one lab cpp)
    -static-libstdc++
    -static-libgcc

--- Cpp/LabbOne/run.bat
+++ Cpp/LabbOne/run.bat
@@ -1,7 +1,7 @@
 @echo off
-@REM cmd /c gcc -c C:\Users\OchiAnthropos\Documents\GitHub\University\Cpp\LabbOne\sqlite3.c -o C:\Users\OchiAnthropos\Documents\GitHub\University\Cpp\LabbOne\sqlite3.o
+cmd /c gcc -c sqlite3.c -static-libgcc -o sqlite3.o
 SET mypath=%~dp0
-cmd /c g++ -c main.cpp -o main.o
-cmd /c g++ sqlite3.o main.o -o program
+cmd /c g++ -c main.cpp -static-libstdc++ -o main.o
+cmd /c g++ sqlite3.o main.o -static-libstdc++ -o program
 cmd /c program
 pause
