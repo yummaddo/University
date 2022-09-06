8106ec01d5df736dbcf7fe64e628a3826cc02f9a
óchiAnthropos <58050924+ochiantropos@users.noreply.github.com>
2022-09-06 17:05:09 +0300

    CompliteCppLab2

--- Cpp/LabbTwo/main.cpp
+++ Cpp/LabbTwo/main.cpp
@@ -4,54 +4,181 @@
 #include <string.h>
 #include <Windows.h>
 #include <math.h>
-
-
-
-struct Product {
-    char* name;
-    char* provider;
-    int numbers;
-    float price;
-};
-
+#include <malloc.h>
 #define is_ua 1
 
+typedef struct Product {
+    char name[24];
+    char provider[18];
+    int numbers;
+    float price;
+    Product* next;
+} Product;
+
+Product * start = NULL;
+
+int end = 1;
+
+
+void display()
+{
+    Product* ptr;
+    if (start == NULL)
+    {
+        return;
+    }
+    else
+    {
+        ptr = start;
+        while (ptr != NULL)
+        {
+            printf("|%23s|%17s|%35d|%9.1f|\n", ptr->name, ptr->provider, ptr->numbers, ptr->price);
+            ptr = ptr->next;
+        }
+    }
+}
+
+
+
+
+void insert_end()
+{
+    Product* temp, * ptr;
+    temp = (Product*)malloc(sizeof(Product));
+    if (temp == NULL)
+    {
+        printf("Out of Memory Space:\n");
+        return;
+    }
+    printf("{Enter the Human Resources Department value} >> ");
+    scanf("%s", &temp->name);
+
+    if (strcmp(temp->name, "exit")) {
+        scanf("%s", &temp->provider);
+        scanf("%d", &temp->numbers);
+        scanf("%f", &temp->price);
+        if (temp->numbers < 0) {
+            printf("Number must be greater than zero\n");
+        }
+        else if (temp->price < 0) {
+            printf("Price must be greater than zero\n");
+        }
+        else if (strlen(temp->name) < 4) {
+            printf("Name must be greater than or equal to 4 characters\n");
+        }
+        else if (strlen(temp->provider) < 4) {
+            printf("Provider must be greater than or equal to 4 characters\n");
+        }
+        else {
+            temp->next = NULL;
+            if (start == NULL)
+            {
+                start = temp;
+            }
+            else
+            {
+                ptr = start;
+                while (ptr->next != NULL)
+                {
+                    ptr = ptr->next;
+                }
+                ptr->next = temp;
+            }
+        }
+
+
+    }
+    else {
+        end = 0;
+    }
+}
+
+
+void create()
+{
+    Product* temp, * ptr;
+    temp = (Product*)malloc(sizeof(Product));
+    if (temp == NULL)
+    {
+        printf("nOut of Memory Space:\n");
+        exit(0);
+    }
+    printf("{Enter the Human Resources Department value} >> ");
+    scanf("%s", &temp->name);
+    if (strcmp(temp->name, "exit")) {
+        scanf("%s", &temp->provider);
+        scanf("%d", &temp->numbers);
+        scanf("%f", &temp->price);
+        if (temp->numbers < 0) {
+            printf("Number must be greater than zero\n");
+        }
+        else if (temp->price < 0) {
+            printf("Price must be greater than zero\n");
+        }
+        else if (strlen(temp->name) < 4) {
+            printf("Name must be greater than or equal to 4 characters\n");
+        }
+        else if (strlen(temp->provider) < 4) {
+            printf("Provider must be greater than or equal to 4 characters\n");
+        }
+        else {
+        
+            temp->next = NULL;
+            if (start == NULL)
+            {
+                start = temp;
+            }
+            else
+            {
+                ptr = start;
+                while (ptr->next != NULL)
+                {
+                    ptr = ptr->next;
+                }
+                ptr->next = temp;
+            }
+        }
+
+    }
+    else {
+        end = 0;
+    }
+
+}
 
 void FirstProgramSecion() {
+    char name[24];
+    char provider[18];
+    int number;
+    float price;
+    printf("{To finish typing, type \"exit\" and press enter}\n");
+    create();
+    while (end) {
+        insert_end();
+    }; // name == exit 
 
-    struct Product data[3] = {
-        {_strdup("Office"), _strdup("Microsoft"), 4,870 },
-        {_strdup("SmartSute"), _strdup("Lotus"), 5,1020 },
-        {_strdup("StarOffice"), _strdup("Sun"), 4,9 }
-    };
     printf(_strdup("-----------------------------------------------------------------------------------------\n"));
     #if is_ua == 1
         printf(_strdup("|Human Resources Department                                                             |\n"));
     #else
         printf(_strdup("|Відділ кадрів                                                                          |\n"));
     #endif
-
     printf(_strdup("-----------------------------------------------------------------------------------------\n"));
-
     #if is_ua == 1
         printf(_strdup("|Name                   | Manufacturer    |Number of components               |Price ($)|\n"));
     #else
         printf(_strdup("|Найменування           |Виробник         |Кількість сост.частин              |Ціна ($) |\n"));
     #endif
-
     printf(_strdup("-----------------------------------------------------------------------------------------\n"));
-    for (int i = 0; i < 3; i++)
-        printf("|%23s|%17s|%35d|%9.1f|\n", data[i].name, data[i].provider, data[i].numbers, data[i].price);
+    display();
     printf(_strdup("-----------------------------------------------------------------------------------------\n"));
-
     #if is_ua == 1
         printf(_strdup("|Notes: Get a free StarOffice product over the Internet                                 |\n"));
     #else
         printf(_strdup("|Примітки: безкоштовно одержати продукт StarOffice через Internet                       |\n"));
     #endif
-    printf(_strdup("-----------------------------------------------------------------------------------------\n"));
+        printf(_strdup("-----------------------------------------------------------------------------------------\n"));
 }
-
 
 void SecondProgramSecion() {
     float x, y;
@@ -60,8 +187,8 @@ void SecondProgramSecion() {
 
     scanf("%f %f", &x, &y);
     printf("------------------------------------------------------------------------------------------\n");
-    for (float a = -5.0f; a < 5; a += 0.25) {
-        for (float b = -5.0f; b < 5; b += 0.25) {
+    for (float a = -5.0f; a < 5; a += 0.5) {
+        for (float b = -5.0f; b < 5; b += 0.5) {
             if (b != a && b != -x && b != x && ((a + x) / (b + x)) > 0) {
                 t1 = (float)(b / ((a - b) * (b + x)) - a / (a - b) * (a - b) * log((a + x) / (b + x)));
                 printf("|%+34.10f|", t1);
@@ -77,22 +204,13 @@ void SecondProgramSecion() {
             else {
                 printf(_strdup("                 invalid values t2|"));
             }
-            printf("  a=%+4.2f",a);
-            printf("  b=%+4.2f |\n",b);
+            printf("  a=%+4.2f", a);
+            printf("  b=%+4.2f |\n", b);
 
         }
     }
     printf("------------------------------------------------------------------------------------------\n");
-
-
 }
-
-
-void ThirdProgramSecion() {
-
-}
-
-
 
 int main() {
     SetConsoleCP(1251); /* налаштувння таблиці кодування вводу*/
--- Cpp/LabbTwo/run.bat
+++ Cpp/LabbTwo/run.bat
@@ -1,12 +1,8 @@
 @echo off
 @REM cmd /c gcc -c sqlite3.c -static-libgcc -o sqlite3.o
 SET mypath=%~dp0
-cmd /c g++ -c main.cpp -static-libstdc++ -o main.o
-cmd /c g++ main.o -static-libstdc++ -o main
+cmd /c gcc -c main.cpp -static-libgcc -o main.o
+cmd /c gcc main.o -static-libgcc -o main
+cmd /c main
 
-chcp 1251
-cd /d "%~dp0"
-cls
-main.exe
-exit /b
 pause
