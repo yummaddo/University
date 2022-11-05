24216ab78e1890ee670bbc5f9b2f19a9cb85c677
óchiAnthropos <58050924+ochiantropos@users.noreply.github.com>
2022-11-05 12:53:59 +0200

    Cpp 5lab 9-8var

---
+++ WorkForOzer/5lab2Curs9var$1-8var$2/5lab2Curs9var$1-8var$2/5lab2Curs9var$1-8var$2.cpp
@@ -0,0 +1,135 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include <Windows.h>
+#include <string.h>
+
+
+#define MAX 200;
+#define MIN 50;
+
+#define NONE         "\033[m"
+#define RED          "\033[0;32;31m"
+
+char* MyRight(char* str, int zip_index) {
+    int num1 = 0;
+    char* temp1;
+    temp1 = str;
+    while (*str != '\0') {
+        num1++;
+        str++;
+    }
+    str--;
+
+    //str -= num1;
+    int size = (num1 % zip_index == 0 ? num1 / zip_index : num1 / zip_index + 1) + num1 +1;
+    char* s = (char*)malloc(sizeof(char) * (size));
+    *(s + size-1) = '\0';
+
+    for (int i = 1; i <= size-1; i++) {
+        if (i == size-1) {
+            *(s + i - 1) = '\n';
+        }
+        else {
+            if (i % (zip_index + 1) != 0) {
+                *(s + i - 1) = *(str);
+                str--;
+            }
+            else {
+                *(s + i - 1) = '\n';
+            }
+        }
+    }
+
+    return s;
+
+}
+
+
+
+
+int main()
+{
+
+    SetConsoleCP(1251); /* налаштувння таблиці кодування вводу*/
+    SetConsoleOutputCP(1251); /* налаштувння таблиці кодування виводу*/
+
+    srand(time(NULL));
+    int* mass1, i, n, m;
+    m = 0;
+
+    int max_el = 0;
+    int max_index = 0;
+
+    int max = MAX;
+    int min = MIN;
+
+    n = min + rand() % (max - min + 1);
+
+    printf(" Розмір масиву: %d\n\n", n);
+    mass1 = (int*)calloc(n, sizeof(int));
+
+    for (i = 0; i < n; i++)
+    {
+    
+        *(mass1 + i) = rand() % 100;
+    
+    }
+
+
+    if (n >= 10) {
+        for (i = 0; i < n-10; i++)
+        {
+            
+            int local_max = 0;
+            for (int inside_i = i; inside_i < i + 10; inside_i++) {
+                local_max += *(mass1 + inside_i);
+            }
+        
+            if (local_max >= max_el) {
+
+                max_el = local_max;
+                max_index = i+1;
+
+            }
+
+        }
+
+    }
+    else {
+        printf("\nМасив немає неперервних ділянок розмірністю 10\n");
+    }
+
+    for (int i = 0; i < n;i++) {
+        if (i >= max_index-1 && i < max_index+9) {
+            printf("%s%3d %s",RED,*(mass1+i),NONE);
+        }
+        else {
+            printf("%3d ", *(mass1 + i));
+        }
+        if ((i+1) % 10 == 0) {
+            printf("\n");
+        }
+ 
+    }
+
+    printf("\nНайбільша неперервна ділянка з %d індексу по %d сума якої рівна %d\n\n", max_index, max_index+9, max_el);
+    printf("\nЗавдання 2, реалізація right(), для завершення сесії введіть . \n");
+    printf("Введіть строку та значення відступу (name 4) \n");
+    char str1[50];
+    int l;
+    do
+    {
+        printf(">> ");
+        scanf("%s", &str1);
+        if (strcmp(str1, ".") == 0) break;
+        scanf("%d", &l);
+        printf("\nРезультат роботи right(str,%d)\n%s\n",l ,MyRight(str1, l));
+    } while (1);
+    
+    return 0;
+}
+
+
+
