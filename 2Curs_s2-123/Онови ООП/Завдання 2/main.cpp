11ba9fd4c6c82ff53e0fcd1af8f6524f33fc81cf
óchiAnthropos <58050924+ochiantropos@users.noreply.github.com>
2023-03-14 14:00:38 +0200

    lab 2

---
+++ 2Curs_s2-123/Онови ООП/Завдання 2/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <chrono>
+
+class Timer {
+private:
+    int m_hour;
+    int m_minute;
+    int m_second;
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_startTime;
+    int* m_data = nullptr;
+    int m_dataSize = 0;
+
+
+public:
+    Timer(int hour, int minute, int second)
+        : m_hour(hour), m_minute(minute), m_second(second),
+          m_startTime(std::chrono::high_resolution_clock::now())
+    {
+        std::cout << "Timer created with time: " << m_hour << "hours : " << m_minute << "minutes : " << m_second << "seconds " << std::endl;
+    }
+
+    ~Timer()
+    {
+        std::chrono::duration<double> elapsed = std::chrono::high_resolution_clock::now() - m_startTime;
+        std::cout << "Timer destroyed. Time elapsed: " << elapsed.count() << " seconds." << std::endl;
+
+        // очищення пам'яті
+        delete[] m_data;
+        m_data = nullptr;
+        m_dataSize = 0;
+    }
+
+    // метод для заповнення масиву даними
+    void fillData(int size)
+    {
+        m_data = new int[size];
+        m_dataSize = size;
+        for (int i = 0; i < size; i++) {
+            m_data[i] = i;
+        }
+    }
+
+
+};
+
+int main()
+{
+    Timer t(1, 2, 3);
+    t.fillData(1000000);
+    // ...
+    return 0;
+} // тут буде викликаний деструктор
