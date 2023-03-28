e557917a7ec3fe653d452b8639a8ceb1882a6e28
óchiAnthropos <58050924+ochiantropos@users.noreply.github.com>
2023-03-28 22:08:00 +0300

    3 lab complit

--- 2Curs_s2-123/Онови ООП/Завдання 3/main_ex_third.cpp
+++ 2Curs_s2-123/Онови ООП/Завдання 3/main_ex_third.cpp
@@ -2,8 +2,38 @@
 
 
 
+class SimpleClass {
+private:
+  int privateField1;
+  double privateField2;
+
+  // Дружній метод для іншого класу
+  friend class FriendClass;
+
+public:
+  SimpleClass(int value1, double value2) : privateField1(value1), privateField2(value2) {}
+
+  void printPrivateFields() {
+    std::cout << "privateField1 = " << privateField1 << ", privateField2 = " << privateField2 << std::endl << std::endl;
+  }
+};
+
+class FriendClass {
+public:
+  static void increasePrivateFields(SimpleClass& obj) {
+    obj.privateField1 += 5; 
+    obj.privateField2 += 5.0;
+  }
+};
+
 int main() {
+  SimpleClass obj(42, 3.14);
+  obj.printPrivateFields(); 
 
+  // Виклик дружнього методу для збільшення приватних полів на порядковий номер журналу
+  FriendClass::increasePrivateFields(obj);
 
+  obj.printPrivateFields(); 
 
+  return 0;
 }
