07fb419829e4bda8b23f9239a4609179240c8e0d
óchiAnthropos <58050924+ochiantropos@users.noreply.github.com>
2023-04-26 23:26:48 +0300

    3 lab complite

--- 2Curs_s2-123/Онови ООП/Завдання 3/main_ex_third.cpp
+++ 2Curs_s2-123/Онови ООП/Завдання 3/main_ex_third.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
 
-
-
-class base
-{ private:
-int x;
-
-public:
-void print( ){std::cout << x  << y;}
+#include <iostream>
+ 
+class Values; // попереднє оголошення класу Values
+ 
+class Display
+{
 private:
-int y;
-
-
+	bool m_displayIntFirst;
+ 
+public:
+	Display() {}
+	
+	void displayItem(Values &value); // вищенаведене попереднє оголошення необхідне для цього рядка
 };
 
-int main(){
+class Values
+{
+private:
+	int m_intValue;
+public:
+	Values(int intValue)
+	{
+		m_intValue = intValue;
+	}
+ 
+	// Робимо метод Display::displayItem() другом класу Values
+	friend void Display::displayItem(Values& value); // помилка: Values не бачить повного визначення класу Display
+};
 
+
+void Display::displayItem(Values &value)
+{
+	std::cout << value.m_intValue << " into >> " <<  value.m_intValue + 5 << '\n';
+    value.m_intValue += 5;
+}
+
+int main()
+{
+    Values value(7);
+    Display display;
+    display.displayItem(value);
+    display.displayItem(value);
+    display.displayItem(value);
+    display.displayItem(value);
+    display.displayItem(value);
+    display.displayItem(value);
 }
